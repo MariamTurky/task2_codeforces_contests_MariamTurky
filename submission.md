0112fd438561d7a23e3aaf7d7923d98c7665b94c
MariamTurky <mariamahmed17>
2021-11-19 12:42:44 +0200

    Initial commit

---
+++ submission.md
@@ -0,0 +1,9 @@
+![](https://i.imgur.com/lCGfPWY.png)
+
+#Frist problem
+###wrong subtraction
+![](https://i.imgur.com/wNM9QB7.png)
+
+#second problem
+###two-gram 
+![](https://i.imgur.com/rL6kdp7.png)
---
+++ two-gram.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int count = 0;
+    int res =0 ;
+    string substring ;
+    for(int i=0; i<n-1; i++){
+        count=0;
+        for(int j=0; j< n-1 ; j++){
+            if ( s[i]==s[j] && s[i+1]==s[j+1]){
+                count ++;
+                if(res<count){
+                    res=count;
+                    substring =string (1, s[i])+ string (1,s[i+1]);
+                }
+            }
+
+
+        }
+
+    }
+    cout << substring<<endl;
+}
